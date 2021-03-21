fdb239325a57a54b9570702365daf29d8fc5ca9a
Simon R <simon@reinisch.io>
2021-03-21 13:45:46 +0100

    Setup PWA, add favicon and manifest file

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,8 @@
 import {resolve} from 'path';
 import {defineConfig} from 'vite';
+import {minifyHtml} from 'vite-plugin-html';
+import {VitePWA} from 'vite-plugin-pwa';
+import manifest from './src/manifest.json';
 
 export default defineConfig({
     esbuild: {
@@ -11,5 +14,12 @@ export default defineConfig({
             '@utils': resolve('./src/utils'),
             '@audio': resolve('./src/audio')
         }
-    }
+    },
+
+    plugins: [
+        minifyHtml(),
+        VitePWA({
+            manifest
+        })
+    ]
 });
