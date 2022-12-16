40c9413fa750b2b58d8bf90e11eb1f9f16b1985f
Simon Reinisch <simon@reinisch.io>
2022-12-16 17:18:46 +0100

    Update dependencies
    Use node v18
    Add vite-plugin-optimize-css-modules plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,33 +1,35 @@
 import preact from '@preact/preset-vite';
-import {VitePWA} from 'vite-plugin-pwa';
+import { defineConfig } from 'vite';
+import { optimizeCssModules } from 'vite-plugin-optimize-css-modules';
+import { VitePWA } from 'vite-plugin-pwa';
 import tsconfigPaths from 'vite-tsconfig-paths';
-import {defineConfig} from 'vite';
 import manifest from './public/manifest.json';
 
 export default defineConfig({
-    esbuild: {
-        target: 'esnext',
-        jsxInject: '',
-        jsxFactory: 'preact',
-        jsxFragment: 'Fragment'
-    },
+  esbuild: {
+    target: 'esnext',
+    jsxInject: '',
+    jsxFactory: 'preact',
+    jsxFragment: 'Fragment',
+  },
 
-    define: {
-        'env.BUILD_TIMESTAMP': JSON.stringify(Date.now())
-    },
+  define: {
+    'env.BUILD_TIMESTAMP': JSON.stringify(Date.now()),
+  },
 
-    server: {
-        port: 3005
-    },
+  server: {
+    port: 3005,
+  },
 
-    plugins: [
-        preact(),
-        tsconfigPaths({loose: true}),
-        VitePWA({
-            registerType: 'autoUpdate',
-            strategies: 'generateSW',
-            base: './',
-            manifest
-        })
-    ]
+  plugins: [
+    optimizeCssModules(),
+    preact(),
+    tsconfigPaths({ loose: true }),
+    VitePWA({
+      registerType: 'autoUpdate',
+      strategies: 'generateSW',
+      base: './',
+      manifest,
+    }),
+  ],
 });
